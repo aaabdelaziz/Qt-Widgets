8df0d048086fd2fbb9bb3e67da0fc1ad0eff012e
aaabdelaziz <aiaaziz.sw@gmail.com>
2025-08-06 20:06:05 +0200

    Update Ok Slot code to show all selected toppings in the Order List

--- Lesson5_CheckBox/dialog.cpp
+++ Lesson5_CheckBox/dialog.cpp
@@ -15,6 +15,22 @@ Dialog::~Dialog()
 
 void Dialog::on_btnOk_clicked()
 {
+    QString topping;
+    // QObjectList boxes = this->children();                        //Create Object list boxes to point to all checkboxes
+    QList<QCheckBox*> boxes = this->findChildren<QCheckBox*>();  //Get the CheckBoxes Only from all the Childrens
+
+    // Loop on all child checkboxes
+    for(QCheckBox* box : boxes)
+    {
+        qDebug() << "A check box is selected";
+        // Check if the check box is existed and is checked, add the checkbox name to the toppings
+        if(box->isChecked())
+            // get the box text name
+            topping += box->text() + "\r\n";
+    }
+
+    //Create user message to show selected toppings
+    QMessageBox::information(this, "Toppings", "Toppings to include\r\n" + topping);
 
 }
 
